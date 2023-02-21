d87532ffacc6752ef1128e6b1ff97dc25c3e0553
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-02-21 19:59:26 +0100

    improved drawCircle function, drow middle point and counted distance

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,6 +3,10 @@ import mediapipe as mp
 import numpy as np
 import time
 import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 
 def putFps(img, pTime):
@@ -16,9 +20,23 @@ def putFps(img, pTime):
     return img, pTime
 
 
-def drawCircle(p):
-    x, y = p[1], p[2]
-    cv2.circle(img, (x, y), 10, (255, 0, 0), cv2.FILLED)
+def drawCircle(p1, p2=None, color=0):
+    if not p2:
+        x, y = p1[1], p1[2]
+    else:
+        x, y = (p1[1] + p2[1])//2, (p1[2] + p2[2])//2
+    if color == 0:
+        RGBcolor = (255, 0, 0)
+    else:
+        RGBcolor = (0, 255, 0)
+    cv2.circle(img, (x, y), 10, RGBcolor, cv2.FILLED)
+
+
+def countDistance(p1, p2):
+    x1, y1 = p1[1], p1[2]
+    x2, y2 = p2[1], p2[2]
+    distance = math.hypot(x2 - x1, y2 - y1)
+    return distance
 
 
 def drawLine(p1, p2):
@@ -39,7 +57,8 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.HandDetector(detectionCon=0.8)
+detector = htm.HandDetector(detectionCon=0.7)
+
 
 
 while True:
@@ -55,7 +74,12 @@ while True:
         """
         drawCircle(lmlist[4])
         drawCircle(lmlist[8])
+        drawCircle(lmlist[4], lmlist[8])
         drawLine(lmlist[4], lmlist[8])
+        distance = countDistance(lmlist[4], lmlist[8])
+        if distance < 50:
+            drawCircle(lmlist[4], lmlist[8], 1)
+
 
     img, pTime = putFps(img, pTime)
     cv2.imshow('Img', img)
