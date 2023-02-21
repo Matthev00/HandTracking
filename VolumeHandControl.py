52ec444adba016b54a3dc1d3abef7f6a1242f755
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-02-21 19:02:32 +0100

    opening camera and printing fps

---
+++ VolumeHandControl.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+import HandTrackingModule as htm
+
+
+def put_fps(img, pTime):
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(
+        img, f'FPS: {int(fps)}', (40, 50),
+        cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
+    return img, pTime
+
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+# detector = htm.HandDetector()
+
+
+while True:
+    succes, img = cap.read()
+
+    img, pTime = put_fps(img, pTime)
+    cv2.imshow('Img', img)
+    cv2.waitKey(1)
