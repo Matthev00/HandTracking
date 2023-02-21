29f643dc486f577566a24c91b1e698090fcd5211
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-02-21 20:10:11 +0100

    added z param to lmlist

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -38,8 +38,8 @@ class HandDetector():
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                lmlist.append([id, cx, cy])
+                cx, cy, cz = int(lm.x * w), int(lm.y * h), int(lm.z * -100)
+                lmlist.append([id, cx, cy, cz])
                 if draw:
                     cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
 
